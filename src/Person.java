ccc6dd746c05b479d818b9c01b6dd29d786aa999
shonmikl <mikhailegorov676@gmail.com>
2023-03-06 16:02:50 +0300

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,18 @@
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    private String name;
+    private Integer age;
+    private Double salary;
+}
+
+class Main1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Postman.java
@@ -0,0 +1,11 @@
+public class Postman {
+    String name;
+
+    void givePost(Post postAddress) {
+        System.out.println("");
+    }
+}
+
+class Post {
+    String address;
+}

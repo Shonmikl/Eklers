022f1e10334fba964079ab59cf9d3c1ef6bf718f
shonmikl <mikhailegorov676@gmail.com>
2023-04-05 16:04:55 +0400

    Node

--- src/_2023_04_05/ListNode.java
+++ src/_2023_04_05/ListNode.java
@@ -12,11 +12,27 @@ public class ListNode {
         this.next = next;
     }
 
+    public ListNode() {
+    }
+
     public boolean hasCycle(ListNode head) {
+
         return true;
     }
 
     public static void main(String[] args) {
+
+        ListNode l5 = new ListNode();
+        ListNode l4 = new ListNode();
+        ListNode l3 = new ListNode();
+        ListNode l2 = new ListNode();
+        ListNode l1 = new ListNode();
+
+        l5.next = l3;
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
 
     }
 }

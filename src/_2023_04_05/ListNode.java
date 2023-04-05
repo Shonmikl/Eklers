4547d3b54b6c87de60249a35c694e0c6467bdfb1
shonmikl <mikhailegorov676@gmail.com>
2023-04-05 15:55:56 +0400

    Node

---
+++ src/_2023_04_05/ListNode.java
@@ -0,0 +1,22 @@
+package _2023_04_05;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListNode {
+    int value;
+    ListNode next;
+
+    public ListNode(int value, ListNode next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public boolean hasCycle(ListNode head) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/_2023_04_05/Student.java
@@ -0,0 +1,17 @@
+package _2023_04_05;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Student {
+    private String name;
+    private int course;
+    private double avgGrade;
+}

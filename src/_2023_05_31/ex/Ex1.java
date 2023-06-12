6713acf9a91ad31bafd15e4c1e9ecc8e7445d2a8
shonmikl <mikhailegorov676@gmail.com>
2023-06-12 14:21:35 +0200

    Exchanger

--- src/_2023_05_31/ex/Ex1.java
+++ src/_2023_05_31/ex/Ex1.java
@@ -1,10 +1,14 @@
 package _2023_05_31.ex;
 
 public class Ex1 {
+    private static final Object LOCK = new Object();
     public int get(int a, int b) {
-        int q1 = m1(a);//m1() return 10
-        int q2 = m2(b);//m2() return 20
-        return q1 + q2;
+        synchronized (LOCK) {
+            int q1 = m1(a);//m1() return 10
+            int q2 = m2(b);//m2() return 20
+            return q1 + q2;
+        }
+
     }
 
     private int m1(int a) {

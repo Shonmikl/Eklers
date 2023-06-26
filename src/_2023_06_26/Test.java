b9f624a55a7b31b5a1568008d842a808f9c395f0
shonmikl <mikhailegorov676@gmail.com>
2023-06-26 14:35:11 +0200

    ---

---
+++ src/_2023_06_26/Test.java
@@ -0,0 +1,12 @@
+package _2023_06_26;
+
+/**
+ * MVC
+ * MVVM
+ * TOF
+ * VIP
+ * VIPER
+ */
+
+public class Test {
+}
---
+++ src/_2023_06_26/viper/Entity.java
@@ -0,0 +1,18 @@
+package _2023_06_26.viper;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Entity представляет структуры данных приложения.
+ * Он может включать классы моделей, хранение данных и
+ * другие объекты, необходимые для представления бизнес-логики.
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+public class Entity {
+    private String username;
+    private String password;
+}
